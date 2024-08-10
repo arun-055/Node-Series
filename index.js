d5b9b3c7fe5014c2eba7ecd1fbcd48db6c59bd7e
codehunt022 <arunnath055@gmail.com>
2024-08-11 02:06:15 +0530

    all

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const PORT = 8000;
-const users = require("./MOCK_DATA.json")
-app.listen(PORT,()=>{console.log("server started")})
+const fs = require("fs");
+const PORT = 3000;
+const users = require("./MOCK_DATA.json");
+//middleware-plugin
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log("hello middlewire 1");
+  // res.json({msg: "hello system "});//to end resopnse from here
+  next();
+});
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "./log.txt",
+    ` ${Date.now()} ${req.method}: ${req.ip}: ${req.path}`,
+    (err, data) => {
+      next();
+    }
+  );
+});
+
+app.use((req, res, next) => {
+  console.log("hello middlewire 1");
+  res.end("stopped from here");
+});
+
+app.use(express.json());
+//Routes
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+app.get("/users", (req, res) => {
+  const html = `
+    <ol>
+    ${users.map((user) => `<li>${user.last_name}</li>`).join("")}
+    </ol> `;
+  res.send(html);
+});
+// //REST API
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  console.log("body", body);
+  fs.writeFile("/MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
+});
+// app.patch("/api/users", (req, res) => {
+//   return res.status({ status: "pending" });
+// });
+
+app.listen(PORT, () => {
+  console.log("server started");
+});
